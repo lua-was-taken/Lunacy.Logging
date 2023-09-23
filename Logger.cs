af203841bb37845a978202c5f0573f25d0b956d6
Lua <uniquesora60@gmail.com>
2023-09-23 17:28:47 +0200

    Increased thread safety for 'Logger.cs'

--- Logger.cs
+++ Logger.cs
@@ -11,92 +11,97 @@ namespace Lunacy.Logging
 
         private static readonly ConcurrentDictionary<ILogger, bool> Handlers = new();
         private static volatile bool Enabled = false;
+        private static readonly object _lock = new();
 
         public static LogEntry Log(string message, LogSeverity severity = LogSeverity.Normal, LogType type = LogType.Info, 
             [CallerFilePath] string? filePath = default, 
             [CallerMemberName] string? memberName = default, 
             [CallerLineNumber] int lineNumber = 0) {
 
-            filePath ??= "UNKN";
-            memberName ??= "UNKN";
+            lock(_lock) { 
+                filePath ??= "UNKN";
+                memberName ??= "UNKN";
 
-            DateTime logTime = DateTime.Now;
+                DateTime logTime = DateTime.Now; //sasd
 
-            AssemblyExplorer.CallerInfo callerInfo = AssemblyExplorer.GetCallerInfo(skipThisAssembly: true);
-            if(callerInfo.Equals(AssemblyExplorer.CallerInfo.Unknown)) {
+                AssemblyExplorer.CallerInfo callerInfo = AssemblyExplorer.GetCallerInfo(skipThisAssembly: true);
+                if(callerInfo.Equals(AssemblyExplorer.CallerInfo.Unknown)) {
 
-                Debug.WriteLine($"Fatal logging error:: Cannot evaluate stack trace, log message: \"{message}\"");
-                return LogEntry.Empty;
-            }
-
-            LogEntry entry = new() {
-                Severity = severity,
-                Type = type,
-                
-                SourceAssembly = callerInfo.AssemblyName,
-                SourceFile = filePath,
-                SourceMember = memberName,
-                SourceLineNumber = lineNumber,
-                LogTime = logTime,
-
-                Message = message
-            };
-
-            foreach((ILogger logger, bool enabled) in Handlers) {
-                if(enabled) {
-                    logger.Handle(entry);
+                    Debug.WriteLine($"Fatal logging error :: Cannot evaluate stack trace, log message: \"{message}\"");
+                    return LogEntry.Empty;
                 }
-            }
 
-            return entry;
+                LogEntry entry = new() {
+                    Severity = severity,
+                    Type = type,
+
+                    SourceAssembly = callerInfo.AssemblyName,
+                    SourceFile = filePath,
+                    SourceMember = memberName,
+                    SourceLineNumber = lineNumber,
+                    LogTime = logTime,
+
+                    Message = message
+                };
+
+                foreach((ILogger logger, bool enabled) in Handlers) {
+                    if(enabled) {
+                        logger.Handle(entry);
+                    }
+                }
+
+                return entry;
+            }
 
         }
 
         public static bool AddLogger(ILogger logger) {
-            return Handlers.TryAdd(logger, true);
+                return Handlers.TryAdd(logger, true);
         }
 
         public static bool AddLogger<T>() where T : ILogger, new() {
-            foreach((ILogger logger, bool _) in Handlers) {
-                if(logger.GetType().IsEquivalentTo(typeof(T))) {
-                    throw new InvalidOperationException("A logger of the same type has already been added. " +
-                        "If this was intentional, please use the explicit AddLogger method.");
+                foreach((ILogger logger, bool _) in Handlers) {
+                    if(logger.GetType().IsEquivalentTo(typeof(T))) {
+                        throw new InvalidOperationException("A logger of the same type has already been added. " +
+                            "If this was intentional, please use the explicit AddLogger method.");
+                    }
                 }
-            }
 
-            return Handlers.TryAdd(new T(), true);
+                return Handlers.TryAdd(new T(), true);
         }
 
         public static T? GetLogger<T>() where T : ILogger, new() {
-            foreach((ILogger logger, bool _) in Handlers) {
-                if(logger.GetType().IsEquivalentTo(typeof(T))) {
-                    return (T)logger;
+                foreach((ILogger logger, bool _) in Handlers) {
+                    if(logger.GetType().IsEquivalentTo(typeof(T))) {
+                        return (T)logger;
+                    }
                 }
-            }
 
-            return default;
+                return default;
         }
 
         public static bool RemoveLogger<T>() where T : ILogger, new() {
-            foreach((ILogger logger, bool _) in Handlers.ToList()) {
-                if(logger.GetType().IsEquivalentTo(typeof(T))) {
-                    return Handlers.Remove(logger, out _);
+                foreach((ILogger logger, bool _) in Handlers.ToList()) {
+                    if(logger.GetType().IsEquivalentTo(typeof(T))) {
+                        return Handlers.Remove(logger, out _);
+                    }
                 }
-            }
 
-            return false;
+                return false;
         }
 
         public static bool RemoveLogger(ILogger logger) {
-            return Handlers.TryRemove(logger, out _);
+                return Handlers.TryRemove(logger, out _);
         }
 
         public static bool Enable() {
-            if(Enabled) {
-                return false;
-            }
+            lock(_lock) {
+                if(Enabled) {
+                    return false;
+                }
 
-            return Enabled = true;
+                return Enabled = true;
+            }
         }
 
         public static bool Enable(ILogger logger) {
@@ -113,12 +118,14 @@ namespace Lunacy.Logging
         }
 
         public static bool Disable() {
-            if(!Enabled) {
-                return false;
-            }
+            lock(_lock) {
+                if(!Enabled) {
+                    return false;
+                }
 
-            Enabled = false;
-            return true;
+                Enabled = false;
+                return true;
+            }
         }
 
         public static bool Disable(ILogger logger) {
